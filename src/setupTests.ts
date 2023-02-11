fb07f1b716b202b742bb4f2b8c0217a6e94a8282
varunsaini21 <varun.saini@squarehealth.com>
2023-02-11 13:10:39 +0530

    not working crypto mock commented

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,7 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// jest.mock('crypto', () => ({
+//   randomUUID: jest.fn(() => Math.random().toString(16)),
+// }));
