bc8f53819033c4867f06e54687acbe1bd1cf7736
varunsaini21 <varun.saini@squarehealth.com>
2023-02-11 17:19:03 +0530

    commit with 1. change range of employee id to 1 to 10 2. uncomment setUser function

--- src/app/screens/RestApi/restApi.ts
+++ src/app/screens/RestApi/restApi.ts
@@ -2,13 +2,12 @@ import renderTemplate from './template';
 import useEmployeeData from './useEmployeeData';
 
 const RestApi: React.FC = () => {
-  const [data, setUserId] = useEmployeeData(1);
+  const { data, setUserId } = useEmployeeData(1);
 
-  function changeUser(e: any) {
-    const id = Math.floor(Math.random() * 10);
-    console.log('id>>>', id);
+  function changeUser() {
+    const id = Math.floor(Math.random() * 10) + 1;
 
-    // setUserId?.(id);
+    setUserId(id);
   }
 
   return renderTemplate({ data, changeUser });
